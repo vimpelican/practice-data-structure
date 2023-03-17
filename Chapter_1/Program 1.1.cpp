9f26783d4e7e8bcc8f3bf2228927b928130c1078
vimpelican <snustcice@g.seoultech.ac.kr>
2023-03-17 16:50:43 +0900

    Add Program 1.1, 1.2

---
+++ Chapter_1/Program 1.1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+// 교재에는 빠져있으나, 추가하였음
+using namespace std;
+
+int main(void)
+{
+    int n=50; float f=20.3;
+    cout << "n: " << n << endl;
+    cout << "f: " << f << endl;
+    return 0;
+}
